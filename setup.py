afe851e66507dfbdf1b6bb60b174fa5733bfe120
Francisco Errandonea <franciscoerrandonea@MacBook-Air-M2-de-Francisco.local>
2024-04-25 14:34:57 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ferrando",
+    version="0.1.0",
+    packages=find_packages(),
+    author="ferrandonea",
+    author_email="tu_email@example.com",
+    description="Utilidades autorreferentes",
+)
