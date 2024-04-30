aa1c1f97039018b0fd0a35cfd4418b9f4fb6f04f
Francisco Errandonea <franciscoerrandonea@MacBook-Air-M2-de-Francisco.local>
2024-04-30 14:14:29 -0400

    primer commit

---
+++ ferrando/fechas/generales.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def named_weekday(input_date: datetime, long: bool = True) -> str:
+    """
+    Obtiene el nombre del día de la semana (en inglés) para una fecha dada. Esto sirve más que nada para debug.
+
+    Args:
+    input_date (datetime): La fecha de la que se quiere obtener el nombre del día.
+    long (bool, optional): Si es True, devuelve el nombre completo del día (e.g., 'Monday'). 
+                           Si es False, devuelve la abreviatura (e.g., 'Mon').
+
+    Returns:
+    str: El nombre del día de la semana de la fecha dada.
+    """
+    date_format = "%A" if long else "%a"
+    return input_date.strftime(date_format)
